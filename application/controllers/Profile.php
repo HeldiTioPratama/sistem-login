65304fa955ad2d1c069beac6f2aa942fe10979d7
HeldiTioPratama <pratamahelditio@gmail.com>
2020-05-04 20:27:44 +0700

    menambahkan menu core, profil dan home untuk user

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,17 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Member extends CI_Controller
+class Profile extends CI_Controller
 {
+
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $data['title'] = 'Home';
+        $data['title'] = 'My profile';
 
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/topbar', $data);
-        $this->load->view('member/index', $data);
+        $this->load->view('profile/index', $data);
         $this->load->view('template/copyright', $data);
         $this->load->view('template/footer');
     }
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -6,43 +6,69 @@
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <?= $roleId = $this->session->userdata['role_id'];
+            if ($roleId == 1) : ?>
+                <!-- Sidebar - Brand -->
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('admin') ?>">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">Administrator</div>
+                </a>
+            <?php else : ?>
+                <!-- Sidebar - Brand -->
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('member') ?>">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">Member</div>
+                </a>
+                <!-- ini home untuk member -->
+                <hr class="sidebar-divider">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('member') ?>">
+                        <i class="fas fa-fw fa-home"></i>
+                        <span>Home</span> </a>
+                </li>
+            <?php endif ?>
+            <!-- Query untuk menampilkan menu -->
+            <?php
+            $queryMenu = "
+                SELECT `user_menu`.`id`, `menu`
+                FROM `user_menu` JOIN `user_access_menu`
+                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                WHERE `user_access_menu`.`role_id` = $roleId
+                ORDER BY `user_access_menu`.`menu_id` ASC
+            ";
+            $menu = $this->db->query($queryMenu)->result_array();
+            ?>
+
+            <!-- Home untuk user -->
+            <?php if ($roleId == 2) : ?>
+
+            <?php endif ?>
+
+
+            <!-- LOOPING menu -->
+            <?php foreach ($menu as $m) : ?>
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    <?= $m['menu'] ?>
                 </div>
-                <div class="sidebar-brand-text mx-3">HTP-login</div>
-            </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Administrator
-            </div>
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                User
-            </div>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-user-circle"></i></i>
-                    <span>My profile</span></a>
-            </li>
-
+                <!-- Query Sub-Menu -->
+                <?php $subMenu = $this->db->get_where('user_sub_menu', ['menu_id' => $m["id"]])->result_array();
+                foreach ($subMenu as $sub) : ?>
+                    <!-- Nav Item - Dashboard -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $sub['url'] ?>">
+                            <i class="<?= $sub['icon'] ?>"></i>
+                            <span><?= $sub['title'] ?></span> </a> </li>
+                <?php endforeach ?>
+            <?php endforeach ?>
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
             <!-- Nav Item - Tables -->
